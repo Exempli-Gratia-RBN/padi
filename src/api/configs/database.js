6e89bbe84b2e74dd2d6a8a428d5e78ac1e208615
Egdevelop <bryandharmawan75@gmail.com>
2024-12-02 08:08:50 +0700

    benerin mongo

--- src/api/configs/database.js
+++ src/api/configs/database.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 import "dotenv/config";
 
-const client = new MongoClient(process.env.MONGODB_URI);
+const client = new MongoClient(process.env.MONGO_URI);
 const dbName = "padi";
 
 export async function connect() {
