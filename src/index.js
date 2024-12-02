27645fee42b5fa9b1dd2e09bd6701ece42807b1f
Egdevelop <bryandharmawan75@gmail.com>
2024-12-02 10:18:57 +0700

    benerin port

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import 'dotenv/config';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import Fastify from 'fastify';
-import ejs from 'ejs';
+import "dotenv/config";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import Fastify from "fastify";
+import ejs from "ejs";
 
-import GLOBAL from './variables.js';
-import { connect } from './api/configs/database.js';
-import './api/services/net.service.js';
+import GLOBAL from "./variables.js";
+import { connect } from "./api/configs/database.js";
+import "./api/services/net.service.js";
 
 await connect();
 
@@ -14,7 +14,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const envToLogger = {
   development: true,
   production: {
-    file: path.join(__dirname, '../logs', 'app.log'),
+    file: path.join(__dirname, "../logs", "app.log"),
     timestamp: true,
   },
   test: false,
@@ -24,32 +24,35 @@ const fastify = Fastify({
   logger: envToLogger[process.env.ENV] ?? true,
 });
 
-fastify.register(import('@fastify/view'), {
+fastify.register(import("@fastify/view"), {
   engine: {
     ejs,
   },
-  root: path.join(__dirname, 'views'),
+  root: path.join(__dirname, "views"),
 });
 
-fastify.register(import('@fastify/static'), {
-  root: path.join(__dirname, '../public'),
+fastify.register(import("@fastify/static"), {
+  root: path.join(__dirname, "../public"),
 });
 
-fastify.get('/', (req, res) => {
-  res.redirect('/platform/detail?id=argasari');
+fastify.get("/", (req, res) => {
+  res.redirect("/platform/detail?id=argasari");
 });
 
 // Register all routes
 for (const route of GLOBAL.ROUTES) {
   fastify.register(import(`./api/routes/${route}.route.js`), {
-    prefix: route === 'page' ? '/' : `/api/${route}`,
+    prefix: route === "page" ? "/" : `/api/${route}`,
   });
 }
 
-fastify.listen({ port: process.env.PORT }, function (err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
+fastify.listen(
+  { host: "0.0.0.0", port: process.env.PORT },
+  function (err, address) {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+    fastify.log.info(`server listening on ${address}`);
   }
-  fastify.log.info(`server listening on ${address}`);
-});
+);
